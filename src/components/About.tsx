986e30dbd4c9cff990f8371a8595aa2d8ba92451
Uday <udaysharma4600@gmail.com>
2025-03-20 14:05:15 +0530

    Landing, About and Tech Stack Page Setup

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+        A Proficient Software Stack Developer 💻 with extensive experience in developing scalable Frontend ⚙️ and backend systems 🗄️ and Full Stack Applications 🚀. Eager to bring a fresh perspective ✨, adaptability 🤝, and enthusiasm 🙌 to a dynamic team 🧑‍🤝‍🧑. I am working on upscaling my skills by learning Advanced Backend development 📚, Gen AI and LLM 🤖. Passionate ❤️‍🔥 and always eager to learn new technologies and frameworks 🛠️ to improve the quality of the Work 💯.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
